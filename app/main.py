f23ec1880765f41d722153fe0997a6f9cdb51cad
OlegGayvoronsky <o.gajvoronskij03@gmail.com>
2024-04-27 21:31:34 +0300

    allcreate

---
+++ app/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from uuid import uuid4
+from pydantic import BaseModel
+import boto3
+import requests
+import json
+import io
+import sys
+import torch
+import numpy as np
+sys.path.append('./app')
+from models import Quesgen, SeqSearch
+
+sys.path.append('')
+
+app = FastAPI()
+raw_model = 'cointegrated/rut5-base-multitask'
+seqsearch = SeqSearch('search.pth', raw_model)
+quesgen = Quesgen('firstmodel.pth', raw_model)
+
+class TextInput(BaseModel):
+    url: str
+
+
+@app.post("/")
+async def Quetion_generation(input: TextInput):
+    url = input.url
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(response.content, 'r') as file:
+                data = np.array(json.load(file))
+        else:
+            HTTPException(status_code=404, detail="Failed to download image")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    choice = list(np.random.choice(len(data), 5, replace=False))
+    data = data[choice]
+    res = dict({'questions': []})
+    for paragraph in data:
+        paragraph = ' '.join(paragraph['content'])
+        context = seqsearch.generate(paragraph, max_length=200)
+        question = quesgen.generate("", context, max_length=128)
+        res['question'].append({
+            'question': question,
+            'title': paragraph['title']
+        })
+
+    return res
---
+++ app/models.py
@@ -0,0 +1,32 @@
+from transformers import (
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+)
+import torch
+
+
+class Quesgen:
+
+    def __init__(self, weights_path, raw_model, device='cpu'):
+        self.model = T5ForConditionalGeneration.from_pretrained(raw_model).to(device)
+        self.tokenizer = T5Tokenizer.from_pretrained(raw_model)
+        self.model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+
+    def generate(self, answer, context, **generator_args):
+        input_ids = self.tokenizer.encode(f'answer: , context: {context}', return_tensors="pt").to(self.model.device)
+        res = self.model.generate(input_ids, **generator_args)
+        output = self.tokenizer.decode(res[0], skip_special_tokens=True)
+        return output
+
+class SeqSearch:
+
+    def __init__(self, weights_path, raw_model, device='cpu'):
+        self.model = T5ForConditionalGeneration.from_pretrained(raw_model).to(device)
+        self.tokenizer = T5Tokenizer.from_pretrained(raw_model)
+        self.model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+
+    def generate(self, input_string, **generator_args):
+        input_ids = self.tokenizer.encode(input_string, return_tensors="pt")
+        res = self.model.generate(input_ids, **generator_args)
+        output = self.tokenizer.decode(res[0], skip_special_tokens=True)
+        return output
